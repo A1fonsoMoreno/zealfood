0f06ecf22e66d5b61ae749c80ccc830f6b6544ec
Alfonso Moreno <alfonso.moreno.am.ma@gmail.com>
2016-02-16 09:09:49 -0800

    commit for i-mac transfer

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,15 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
   def create
-    
+    product = Product.new(product_params)
+    if product.save
+      redirect_to new_product_path, notice: "Great Job"
+    else
+      render :new
+    end
+  end
+  private
+  def product_params
+    params.require(:product).permit(:name, :category)
+  end
 end
--- test/features/create_new_product_test.rb
+++ test/features/create_new_product_test.rb
@@ -6,7 +6,6 @@ class CreateNewProductTest < Capybara::Rails::TestCase
     fill_in "Name", with: "Ausie"
     select "Food", from: "Category"
     click_on "Create Product"
-    save_and_open_page
     within "#flash-messages" do
       assert page.has_text?("Great Job")
     end
